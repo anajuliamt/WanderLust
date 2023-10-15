2d409b1ccfa91ff7946e977589d1941cd1f9f8d7
Grazielly <graziellycosta396@gmail.com>
2023-10-15 14:41:24 -0300

    Arrumando coisas

--- cadastro.js
+++ cadastro.js
@@ -2,5 +2,7 @@ function mostrarOcultarSenha(){
     var senha=document.getElementById("senha");
     if(senha.type=="password"){
         senha.type="text";
+    } else if (senha.type=="text"){
+        senha.type="password";
     }
 }
